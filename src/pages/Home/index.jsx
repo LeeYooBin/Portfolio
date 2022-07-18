29cb384855b84111eb2980718c39353000de7e75
LeeYooBin <santosroberdan25@gmail.com>
2022-07-18 19:57:39 -0300

    Configurações iniciais do projeto

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "../../style/Global";
+import Header from "../../components/Header";
+
+export default function Home(){
+    return(
+        <>
+            <GlobalStyle />
+            <Header />
+        </>
+    );
+}
