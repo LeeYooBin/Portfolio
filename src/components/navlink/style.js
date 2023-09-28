94f3b1bd21df5a38c480ef949c77ebd5f4957769
LeeYooBin <santosroberdan25@gmail.com>
2023-09-28 01:23:53 -0300

    Creating and styling Navlink component

---
+++ src/components/navlink/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+export const Content = styled.a`
+  text-decoration: none;
+  font-size: ${theme.fonts.text};
+  color: ${theme.colors.text};
+  font-weight: 500;
+`;
