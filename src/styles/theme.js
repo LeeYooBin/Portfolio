6cfc4a30996f84e0f2cb78d22dc0c513710f65c2
LeeYooBin <santosroberdan25@gmail.com>
2023-09-28 01:20:44 -0300

    Setting theme and GlobalStyle

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    background: '#0e0d0d',
+    text: '#FEFEFE',
+    project: '#363636',
+  },
+  fonts: {
+    heading: '2.6rem',
+    title: '1.95rem',
+    projectTitle: '1.52rem',
+    projectText: '1.3rem',
+    text: '1.56rem',
+  },
+};
+
+export default theme;
