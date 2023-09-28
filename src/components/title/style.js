e45c5593ae082f57416556b74cbc8ce0ac4f08ee
LeeYooBin <santosroberdan25@gmail.com>
2023-09-28 01:21:49 -0300

    Creating and styling Title

---
+++ src/components/title/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+export const Content = styled.h2`
+  color: ${theme.colors.text};
+  font-size: ${theme.fonts.title};
+  font-weight: 700;
+  margin: 2.5rem 0;
+`;
